3e3f80cdca427ca6c54488ae25720527a3fee358
stellarn <stellern@gmail.com>
2016-06-06 13:32:26 +0300

    work in progress

---
+++ genericTree.c
@@ -0,0 +1,177 @@
+//
+//  genericTree.c
+//  bankMTM
+//
+//  Created by Noam Stolero on 6.6.2016.
+//  Copyright © 2016 Noam Stolero. All rights reserved.
+//
+
+#include "genericTree.h"
+#include "common.h"
+
+
+
+/*create tree node*/
+genTree * create_Tree(){
+    
+    genTree *temp;
+    
+    temp=ALLOC(genTree, 1);
+    temp->data=NULL;
+    temp->left=NULL;
+    temp->right=NULL;
+    return temp;
+}
+
+
+/*add node to tree*/
+genTree * add_new_node(genTree* t,void* data,genCmp cmp){
+    comparison c;
+    if(!t){
+        t=create_Tree();
+        t->data=data;
+        return t;
+    }
+    c=cmp(t->data,data);
+    if (c==GREATER){
+        t->right=add_new_node(t->right, data, cmp);
+    }else{
+        t->left=add_new_node(t->left, data, cmp);
+    }
+    
+    return t;
+}
+
+
+/* find biggest node in given tree */
+genTree * findBiggestNode(genTree* t){
+  
+    while (t!=NULL || t->right!=NULL){
+        t=t->right;
+    }
+    return t;
+}
+
+/* find smallestNode in given tree */
+genTree * findSmallestNode(genTree* t){
+    
+    while (t!=NULL || t->right!=NULL){
+        t=t->left;
+    }
+    return t;
+}
+
+
+/* delete a leaf from tree */
+genTree * deleteLeaf(genTree *t, genDelete gDelete){
+    gDelete(t->data);
+    return NULL;
+}
+
+
+
+/* remove a node from tree */
+genTree * remove_node(genTree* t, void* data, genDelete gDelete, genCmp cmp){
+    genTree* toSwap, *toDelete, *parent=NULL;
+    void *temp;
+    
+    if (!t){
+        return NULL;
+    }
+    
+    toDelete=find_Node_Parent(t, data,&parent, cmp);
+    
+    if (toDelete==NULL)
+        return NULL;
+    
+    
+    if(toDelete->left==NULL && toDelete->right!=NULL){
+        toSwap=findSmallestNode(toDelete->right);
+        temp=toDelete->data;
+        toDelete->data=toSwap->data;
+        toSwap->data=temp;
+        toDelete->right=remove_node(toDelete->right,data, gDelete, cmp);
+        return toDelete;
+    }else if (toDelete->left!=NULL){
+        toSwap=findBiggestNode(toDelete->left);
+        temp=toDelete->data;
+        toDelete->data=toSwap->data;
+        toSwap->data=temp;
+        toDelete->left=remove_node(toDelete->left, data,gDelete, cmp);
+        return toDelete;
+    }
+    
+    if (parent){
+        if (parent->left==toDelete)
+            parent->left=NULL;
+        else
+            parent->right=NULL;
+        deleteLeaf(toDelete->data, gDelete);
+        FREE(toDelete);
+        return t;
+    }
+    
+    
+    deleteLeaf(toDelete->data, gDelete);
+    FREE(toDelete);
+    return NULL;
+}
+
+
+
+
+
+
+genTree * find_Node_Parent(genTree * ,void* ,genTree **,genCmp cmp){
+    
+}
+
+
+
+
+
+
+
+/* create linked list from a certain data node */
+genLinked * find_node(genTree* t, void* data, genCmp cmp)
+{
+    genLinked *left, *right;
+    if(!t)
+        return NULL;
+    left=find_node(t->left, data, cmp);
+    right=find_node(t->right, data, cmp);
+    
+    if(cmp(t->data,data)==EQUAL){
+        genLinked *me;
+        me = ALLOC(genLinked, 1);
+        me->data = t->data;
+        right = merge_LinkedList(me,right);
+    }
+    
+    return merge_LinkedList(left,right);
+    
+}
+
+
+/* merge two linked list */
+genLinked* merge_LinkedList(genLinked* a,genLinked* b)
+{
+    genLinked* new=a;
+    if(a==NULL)
+        return b;
+    if(b==NULL)
+        return a;
+    while(a->next){
+        a=a->next;
+    }
+    a->next=b;
+    return new;
+}
+
+
+
+
+
+
+
+
---
+++ genericTree.h
@@ -0,0 +1,65 @@
+//
+//  genericTree.h
+//  bankMTM
+//
+//  Created by Noam Stolero on 6.6.2016.
+//  Copyright © 2016 Noam Stolero. All rights reserved.
+//
+
+#ifndef genericTree_h
+#define genericTree_h
+
+#include <stdio.h>
+
+#endif /* genericTree_h */
+
+
+
+typedef struct genLinkedLsits{
+    void *data;
+    struct genLinkedLsits *next;
+}genLinked;
+
+
+typedef struct genTrees{
+    
+    void * data;
+    struct genTrees* left;
+    struct genTrees* right;
+
+}genTree;
+
+typedef enum{SMALLER=-1, EQUAL=0, GREATER=1}comparison;
+
+typedef int (*genCmp)(void*,void*);
+typedef void (*genDelete)(void*);
+
+
+/*------------------------GENERAL LINKED LIST--------------------*/
+
+genLinked* merge_LinkedList(genLinked*,genLinked*);
+
+genLinked * find_node(genTree*, void*, genCmp);
+
+/*------------------------GENERAL LINKED LIST--------------------*/
+
+genTree * create_Tree();
+
+genTree * add_new_node(genTree*,void*, genCmp);
+
+genTree * remove_node(genTree*,void*,genDelete, genCmp);
+
+
+genTree * find_Node_Parent(genTree *,void* ,genTree **,genCmp cmp);
+
+double average_key(genTree*, genValue);
+
+genTree * tree_to_array(genTree*);
+
+void print_List(genTree*, genPrintList);
+
+void free_list(genTree*);
+
+
+
+
