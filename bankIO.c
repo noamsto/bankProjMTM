77d70279bb500b50084124ecffc7953bc69b3d6c
yali <Yalits4@gmail.com>
2016-06-18 13:41:03 +0300

    new bank input output file.

---
+++ bankIO.c
@@ -0,0 +1,8 @@
+/*
+ * bankIO.c
+ *
+ *  Created on: Jun 18, 2016
+ *      Author: yali
+ */
+
+
---
+++ bankIO.h
@@ -0,0 +1,13 @@
+/*
+ * bankIO.h
+ *
+ *  Created on: Jun 18, 2016
+ *      Author: yali
+ */
+
+#ifndef BANKIO_H_
+#define BANKIO_H_
+
+
+
+#endif /* BANKIO_H_ */
